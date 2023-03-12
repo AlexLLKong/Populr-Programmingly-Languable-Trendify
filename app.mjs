f78a89277a105a625e97d88f85eec160219e334d
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-03-12 00:49:05 -0800

    add descriptions to each language

--- app.mjs
+++ app.mjs
@@ -9,57 +9,57 @@ const languages = [
 	{
 		name: "Python",
 		mCode: "05z1_",
-		description: "Description of Python"
+		description: "Python is a high-level, dynamic-typed and interpreted programming language that is used in a wide variety of applications, from web development to machine learning to scientific computing. Its simplicity, readability, and wide range of libraries and tools make it an excellent choice for both beginner and experienced programmers."
 	},
 	{
 		name: "C",
 		mCode: "01t6b",
-		description: "Description of C"
+		description: "C is a high-level compiled programming language that is widely used for systems programming, embedded systems, game development, and scientific computing. Its low-level control, portability, and efficiency make it an excellent choice for developers who need to develop fast and efficient code for a wide variety of platforms."
 	},
 	{
 		name: "C++",
 		mCode: "0jgqg",
-		description: "Description of C++"
+		description: "C++ is a high-level, general-purpose, programming languagethat is an extension of the C. It is widely used for developing software applications of all types. Its support for object-oriented programming, speed and efficiency, and low-level control make it an excellent choice for developers who need to develop high-performance applications for a wide variety of platforms."
 	},
 	{
 		name: "C#",
 		mCode: "07657k",
-		description: "Description of C#"
+		description: "C# is a modern, high-level programming language that was developed by Microsoft in the early 2000s. It is widely used for developing applications for the Microsoft .NET Framework and related technologies. Its support for object-oriented programming, type safety, garbage collection, and platform independence make it an excellent choice for developers who need to develop high-quality applications for a wide variety of platforms."
 	},
 	{
 		name: "HTML",
 		mCode: "03g20",
-		description: "Description of HTML"
+		description: "HTML (Hypertext Markup Language) is a markup language used to create and design web pages. It is a foundational technology for the World Wide Web and is essential for creating and displaying content on websites."
 	},
 	{
 		name: "CSS",
 		mCode: "015tjh",
-		description: "Description of CSS"
+		description: "CSS (Cascading Style Sheets) is a style sheet language used to describe the presentation and styling of HTML (Hypertext Markup Language) and XML (Extensible Markup Language) documents. It is used to define the visual appearance of web pages, including layout, color, font, and other stylistic elements."
 	},
 	{
 		name: "JavaScript",
 		mCode: "02p97",
-		description: "Description of JavaScript"
+		description: "JavaScript is a high-level, interpreted programming language that is commonly used for creating interactive web pages and applications. It is a client-side scripting language, which means that it runs within the web browser of the user rather than on a web server."
 	},
 	{
 		name: "Kotlin",
 		mCode: "0_lcrx4",
-		description: "Description of Kotlin"
+		description: "Kotlin is a modern, open-source programming language that runs on the Java Virtual Machine (JVM) and can be used to develop a wide range of applications, including Android mobile apps, web applications, desktop applications, and server-side applications."
 	},
 	{
 		name: "SQL",
 		mCode: "075st",
-		description: "Description of SQL"
+		description: "SQL (Structured Query Language) is a programming language used to manage and manipulate data stored in relational databases. It is used to create, modify, and delete databases, tables, and data within those tables."
 	},
 	{
 		name: "Assembly",
 		mCode: "0p8g",
-		description: "Description of Assembly"
+		description: "Assembly language is a low-level programming language that is used to write programs for specific hardware architectures. It is often used to write system software, such as device drivers and operating systems, as well as firmware for embedded systems."
 	},
 	{
 		name: "PHP",
 		mCode: "060kv",
-		description: "Description of PHP"
+		description: "PHP (Hypertext Preprocessor) is a server-side scripting language used to develop dynamic web applications. It is widely used for building websites and web applications that interact with databases and other web-based services."
 	}
 	
 ]
